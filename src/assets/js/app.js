e1a6ad17c4f29f303bab6b21863616f587c527d5
Misha Pliskunov <pliskunov.mykhailo@gmail.com>
2018-01-24 22:37:57 +0200

    init

---
+++ src/assets/js/app.js
@@ -0,0 +1,67 @@
+//=require jquery.min.js
+
+//=require what-input.min.js
+
+// Core Foundation files
+//=require foundation.core.min.js"
+//=require foundation.util.*.min.js"
+
+//=require foundation.dropdown.min.js"
+//=require foundation.responsiveMenu.min.js"
+//=require foundation.responsiveToggle.min.js"
+//=require foundation.toggler.min.js"
+
+// to inlude foundation plugins add "=" sign below
+//
+// require foundation.abide.min.js"
+// require foundation.accordion.min.js"
+// require foundation.accordionMenu.min.js"
+// require foundation.drilldown.min.js"
+// require foundation.dropdownMenu.min.js"
+// require foundation.equalizer.min.js"
+// require foundation.interchange.min.js"
+// require foundation.magellan.min.js"
+// require foundation.offcanvas.min.js"
+// require foundation.orbit.min.js"
+// require foundation.reveal.min.js"
+// require foundation.slider.min.js"
+// require foundation.sticky.min.js"
+// require foundation.tabs.min.js"
+// require foundation.tooltip.min.js"
+// require foundation.zf.responsiveAccordionTabs.min.js"
+
+//=require slick.min.js
+
+
+;(function($){
+	$(document).foundation();
+
+	$(document).ready(function(){
+		//var $slider = $('.ba-slider');
+
+		// $slider.slick({
+		// 	dots: true,
+		// 	infinite: false
+		// });
+	});
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
